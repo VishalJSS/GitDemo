60489546f668ab7dad947eee7119b2bc231bc57f
Vishal J S <vishalgeethaeee@gmail.com>
2023-10-10 15:46:09 +0530

    commit branch

--- learn_xpath.py
+++ learn_xpath.py
@@ -8,6 +8,8 @@ driver.get("https://www.google.co.in")
 driver.find_element(By.XPATH,"//a[contains(@aria-label,'Google apps')]").click()
 time.sleep(10)
 #Somthing
+#Branching
+#Testing
 #Otherthing
 #Nothing
 driver.find_element(By.XPATH,"//span[@data-text='YouTube']").click()
